6680f1f76e57c8b240602cb8c79b83100a3e6f7a
peisenfang <52929994+peisenfang@users.noreply.github.com>
2022-05-08 16:43:28 +0800

    Add files via upload

--- mi/js/index.js
+++ mi/js/index.js
@@ -8,104 +8,95 @@ window.onload=function(){
     var list=document.getElementsByClassName("list");
     var tool=document.getElementsByClassName("hover")
     var tools=document.getElementsByClassName("tools")   
-    var index=0;
-    var index1=0;
-    var index2=0;
     var timer=null;
 // 上方商品列表切换
-var clearclass1=function(){
+function clearclass1(){
     for(let i=0;i<list.length;i++){
         list[i].className="list";
         list[i].setAttribute("number",i);
-     }
+    }
 }
 clearclass1();
-function move(){
-    clearclass1();
-    list[index].className="list active";
-    }
 for(let i=0;i<list.length;i++){
     show_goods[i].addEventListener("mouseover",function(){
-        let list_index=list[i].getAttribute("number");
-        index=list_index;
-        move();
+        let index=list[i].getAttribute("number");
+        clearclass1();
+        list[index].className="list active";
     })
 }
 
 // 轮播图效果
-var clearclass2=function(){
+let index_img = 0;
+function clearclass2(){
     for(let i=0;i<imgs.length;i++){
         imgs[i].className="xximg";
         circles[i].className="circle";
         circles[i].setAttribute("num",i);
     }
 }
-function move1(){
+function move(){
     clearclass2();
-    imgs[index1].className="xximg active";
-    circles[index1].className="circle circle-active";
+    imgs[index_img].className="xximg active";
+    circles[index_img].className="circle circle-active";
 }
-// 点击小圆点切换轮播图
-for(let i=0;i<circles.length;i++){
+move();
+for(let i=0;i<circles.length;i++){// 点击小圆点切换轮播图
     circles[i].addEventListener("click",function(){
-        let circle_index=circles[i].getAttribute("num");
-        index1=circle_index;
-        move1();
+        index_img = circles[i].getAttribute("num");
+        move();
     })
 }
-// 切换至下一张轮播图
-nextBtn.onclick=function(){
-    if(index1<imgs.length-1){
-        index1++;
+
+nextBtn.onclick=function(){// 切换至下一张轮播图
+    if(index_img<imgs.length-1){
+        index_img++;
     }
     else{
-        index1=0;
+    index_img=0;
     }
-    move1();
+    move();
 }
-//  切换下一张轮播图
-preBtn.onclick=function(){
-    if(index1>0){
-        index1--;
+
+preBtn.onclick=function(){//  切换下一张轮播图
+    if(index_img>0){
+        index_img--;
     }
     else{
-        index1=imgs.length-1;
+        index_img=imgs.length-1;
     }
-    move1();
+    move();
 }
-// 轮播图计时轮换
-timer=setInterval(
+
+timer=setInterval(// 轮播图计时轮换
     function(){
         nextBtn.onclick();}
 ,2000)
-// 鼠标移入时停止计时
-banner.onmouseover=function(){
+
+banner.onmouseover=function(){// 鼠标移入时停止计时
     clearInterval(timer);
 } 
-// 鼠标移出继续计时
-banner.onmouseleave=function(){
+
+banner.onmouseleave=function(){// 鼠标移出继续计时
     clearInterval(timer);
     timer=setInterval(
         function(){
             nextBtn.onclick();}
     ,2000)
 }
+
 // 右侧工具栏
-var clearclass3=function(){
+function clearclass3(){
     for(let i=0;i<tool.length;i++){
         tool[i].className="hover";
         tool[i].setAttribute("number1",i);
      }
 }
-function move2(){
-    clearclass3();
-    tool[index2].className="hover visible";
-    }
+
 for(let i=0;i<tool.length;i++){
     tools[i].addEventListener("mouseover",function(){
-        let tool_index=tool[i].getAttribute("number1");
-        index2=tool_index;
-        move2();
+        let index_tool=tool[i].getAttribute("number1");
+        clearclass3();
+        tool[index_tool].className="hover visible";
     })
 }
 for(let i=0;i<tool.length;i++){
